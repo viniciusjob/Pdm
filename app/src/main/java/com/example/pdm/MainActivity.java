f92e51fa3b7d412999b6d6cf040c7917a05d1ea8
Vinícius Job <viniciusgjob@gmail.com>
2020-03-11 19:13:48 -0300

    Adicionada conversão de graus para farenheidt

--- app/src/main/java/com/example/pdm/MainActivity.java
+++ app/src/main/java/com/example/pdm/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.pdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btn = (Button) findViewById(R.id.btnButton);
         btn.setOnClickListener(this);
+
+//        edt.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//
+//            }
+//        });
     }
 
     @Override
@@ -30,19 +52,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
+
         Log.d("CICLO", "onResume");
     }
 
-    public void clickButton(View view) {
-        i++;
-        Log.d("Click", "Você Clicou " + i + " vezes");
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("CICLO", "onPause");
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("CICLO", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("CICLO", "onDestroy");
+    }
+
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnButton:
                 Log.d("Click", "Click por Switch");
+
+                EditText edt = (EditText) findViewById(R.id.editText);
+                TextView txtView = (TextView) findViewById(R.id.txtView);
+                String input;
+                input = edt.getText().toString();
+                if (input == "" || input.isEmpty()) {
+
+                } else {
+                    int inputInt = Integer.parseInt(input);
+                    int result = (((inputInt * 9) / 5) + 32);
+                    if (txtView.getText().toString().length() < 11){
+                        txtView.setText(txtView.getText().toString() + " " + result);
+                    } else {
+                        txtView.setText("Resultado: " + result);
+                    }
+                }
         }
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
     }
 }
